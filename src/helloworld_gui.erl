c5b06adf4ae1d4a52dd5c0d43ca8b20618ec70f3
Yangalor <yangalor@gmail.com>
2017-04-26 11:02:23 +0300

    Рефакторинг кода, добавление комментариев

--- src/helloworld_gui.erl
+++ src/helloworld_gui.erl
@@ -10,7 +10,7 @@
 -behavior(wx_object).
 
 
-%% API
+%% Блок объявления API
 
 -export([start/0, start/1, start_link/0, start_link/1, 
 		 init/1, handle_call/3, handle_cast/2, handle_event/2,
@@ -19,7 +19,7 @@
 -export([timeupdate/0]).
 		 
            
-%% API
+%% Блок реализации базового API
 
 start() ->
     start([]).
@@ -33,24 +33,30 @@ start_link() ->
 start_link(Debug) ->
     wx_object:start_link({local, ?MODULE}, ?MODULE, Debug, []).
 
-%% Callbacks
+%% Блок реализации API wx_object
 
 init(Args) ->
-	
+	% Инициализация GUI
 	Wx = wx:new(Args),	
 	
+	% Перехват сообщений от связанных процессов
 	process_flag(trap_exit, true),
 	
+	% Создание главного окна приложения
 	Frame = wxFrame:new(Wx, ?wxID_ANY, "Привет, Мир!", [{size, {800, 600}}]),
 	
+	% Создание и настройка статусной строки из трех секций
 	StatusBar = wxFrame:createStatusBar(Frame, []),	
 	wxStatusBar:setFieldsCount(StatusBar, 3, [{widths, [-1, -1, 100]}]),	
 	wxStatusBar:setStatusStyles(StatusBar, [?wxSB_FLAT, ?wxSB_FLAT, ?wxSB_NORMAL]),
 	
+	% Запуск таймера обновления системного времени в статусной строке GUI
 	timer:apply_interval(1000, ?MODULE, timeupdate, []),
-		
+	
+	% Отображение главного окна приложения на экране	
 	wxFrame:show(Frame),
 	
+	% Возврат главного окна приложения и статуса GUI
 	{Frame, {Frame}}.
 
 code_change(_, _, State) ->
@@ -58,6 +64,8 @@ code_change(_, _, State) ->
 	
 terminate(_Reason, _State) ->
 	wx:destroy().
+
+%% Блок обработки сообщений от процессов
 
 handle_info({'EXIT', _, wx_deleted}, State) ->
     {noreply,State};
@@ -68,22 +76,31 @@ handle_info({'EXIT', _, normal}, State) ->
 handle_info(_Msg, State) ->    
     {noreply,State}.
 	
-handle_call(_Msg, _From, State) ->    
+%% Блок обработки синхронных вызовов
+	
+handle_call(_Msg, _From, State) ->  			% Обработка прочих синхронных вызовов  
     {reply,ok,State}.	
 
-handle_cast(timeupdate, State = {Frame}) ->
-	StatusBar = wxFrame:getStatusBar(Frame),
+%% Блок обратоки асинхронных вызовов
+
+handle_cast(timeupdate, State = {Frame}) ->		% Обновление времени по таймеру в статусной строке GUI
+	% Получение статусной строки GUI
+	StatusBar = wxFrame:getStatusBar(Frame),	
+	
+	% Вывод отформатированного отображения времени в третью секцию статусной строки GUI
 	wxStatusBar:pushStatusText(StatusBar, io_lib:format("Время ~2.10.0b:~2.10.0b:~2.10.0b", tuple_to_list(time())), [{number, 2}]),
+	
 	{noreply,State};
-handle_cast(_Msg, State) ->    
+handle_cast(_Msg, State) ->    					% Обработка прочих асинхронных вызовов
     {noreply,State}.
 
-handle_event(#wx{event=#wxClose{}}, State) ->        
+handle_event(#wx{event=#wxClose{}}, State) ->	% Обработка события закрытия главного окна GUI
     {stop, normal, State};
-handle_event(_,State) ->    
+handle_event(_,State) ->    					% Обработка прочих событий
     {noreply, State}.           
 
-%% Internals
+%% Блок внутренних процедур и функций
 
-timeupdate() -> 
+timeupdate() -> 								% Интерфейс вызова обновления времени в статусной строке GUI
 	wx_object:cast(?MODULE, timeupdate).
+
